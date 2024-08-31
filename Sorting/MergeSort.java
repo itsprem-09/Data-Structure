742a557139bb84c391f030d82adccecaf003feae
Prem Jani <143021268+itsprem-09@users.noreply.github.com>
2024-08-31 12:51:58 +0530

    Add files via upload

---
+++ Sorting/MergeSort.java
@@ -0,0 +1,72 @@
+
+
+//Time complexity:
+//BEST : O(N log(N))
+//AVG : O(N log(N))
+//WORST : O(N2)
+//Space Complexity: O(N) + O(N) auxiliary stack space.
+
+// divide and conquers algorithm
+
+//Approach : Devide the array into two parts and arrange them in sorted way
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {2,1,4,1,-5,2,3};
+        mergeSort(arr, 0, arr.length-1);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+    public static void mergeSort(int[] arr,int low,int high){
+        if (low >= high) {
+            return;
+        }
+        int mid = (low+high)/2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid+1, high);
+        merge(arr,low,mid,high);
+    }
+
+    public static void merge(int[] arr,int low,int mid,int high){
+        int leftPointer = low;
+        int rightPointer = mid+1;
+        int[] temp = new int[arr.length];
+        
+        int tempIterator = low;
+
+        while (leftPointer <= mid && rightPointer <= high) {
+            if (arr[leftPointer] <= arr[rightPointer]) {
+                temp[tempIterator] = arr[leftPointer];
+                leftPointer++;
+            }
+            else{
+                temp[tempIterator] = arr[rightPointer];
+                rightPointer++;
+            }
+            tempIterator++;
+        }
+
+        //check if any element left in left or right seperated array
+        if (leftPointer > mid) {
+            for (int i = rightPointer; i <= high; i++) {
+                temp[i] = arr[i];
+                tempIterator++;
+            }
+        }
+        else{
+            for (int i = leftPointer; i <= mid; i++) {
+                temp[tempIterator] = arr[i];
+                tempIterator++;
+            }
+        }
+
+        //replace orignal array to sorted one
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,44 @@
+
+
+//Time complexity:
+//BEST : O(N2)
+//AVG : O(N2)
+//WORST : O(N2)
+//Space Complexity: O(1)
+
+//Approach : Find Minimum Element's index and swap that element with current Element
+//           (Push the minimum element from array at first)
+public class SelectionSort {
+    static void selection_sort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int mini = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[mini]) {
+                    mini = j;
+                }
+            }
+            //swap
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("After selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {13, 46, 24, 52, 20, 9};
+        int n = arr.length;
+        System.out.println("Before selection sort:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        selection_sort(arr, n);
+    }
+}
