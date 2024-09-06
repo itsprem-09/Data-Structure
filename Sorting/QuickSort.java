7233bc1a799709360c62fa564e0853e7da046b2c
Prem Jani <143021268+itsprem-09@users.noreply.github.com>
2024-09-06 22:09:47 +0530

    Add files via upload

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,53 @@
+
+//Pivot Element : Which Element in Array where left Side of Element is lesser than and right side of element is greater than it. initally we declare first element as pivot element
+
+//Time complexity:
+//BEST : O(N log(N))
+//AVG : O(N log(N))
+//WORST : O(N2)
+//Space Complexity: O(1) + O(N) auxiliary stack space.
+
+// Worst Case – This case occurs when the pivot is the greatest or smallest element of the array. If the partition is done and the last element is the pivot, then the worst case would be either in the increasing order of the array or in the decreasing order of the array.
+
+//Approach : Pick a pivot and place it in its correct place in the sorted array.
+//           Shift smaller elements(i.e. Smaller than the pivot) on the left of the pivot and larger ones to the right.
+
+public class QuickSort {
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while (i < j) {
+            while (arr[i] <= pivot && i <= high - 1) {
+                i++;
+            }
+
+            while (arr[j] > pivot && j >= low + 1) {
+                j--;
+            }
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+
+    static void qs(int[] arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            qs(arr, low, pIndex - 1);
+            qs(arr, pIndex + 1, high);
+        }
+    }
+    public static int[] quickSort(int[] arr) {
+        // Write your code here.
+        qs(arr, 0, arr.length - 1);
+        return arr;
+    }
+}
---
+++ Sorting/ShellSort.java
@@ -0,0 +1,69 @@
+
+import java.util.Arrays;
+
+// It is Optimization of Interstion Sort
+// When gap will became 1 it will run the insertion sort. 
+
+//Time complexity:
+//BEST : O(N * log(N))
+//AVG : O(N * (log(N))^2)
+//WORST : O(N2)
+//Space Complexity: O(1)
+
+// Approach : Initally declare a gap of n/2 where n is size of an array. and run that loop till gap became 1.
+// After that check element that which has index difference = gap
+// and swap them if it is bigger than it.(Smaller element should come earlier)
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = {2 , 4 , 1 , 9 , -1 , 3 , 0 , 1};
+        shellSortPPT(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void shellSort(int[] arr){
+        int n = arr.length;
+
+        // Start with a big gap, then reduce the gap
+        for (int gap = n/2; gap > 0; gap /= 2)
+        {
+            // Do a gapped insertion sort for this gap size.
+            // The first gap elements a[0..gap-1] are already
+            // in gapped order keep adding one more element
+            // until the entire array is gap sorted
+            for (int i = gap; i < n; i += 1)
+            {
+                // add a[i] to the elements that have been gap
+                // sorted save a[i] in temp and make a hole at
+                // position i
+                int temp = arr[i];
+
+                // shift earlier gap-sorted elements up until
+                // the correct location for a[i] is found
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+
+                // put temp (the original a[i]) in its correct
+                // location
+                arr[j] = temp;
+            }
+        }
+    }
+
+    public static void shellSortPPT(int[] arr){
+        int n = arr.length;
+        for (int gap = n/2; gap >= 1; gap/=2) {
+            for (int j = gap; j < n; j++) {
+                for (int i = j - gap; i >= 0; i-=gap) {
+                    if (arr[i + gap] > arr[i]) {
+                        break;
+                    }
+                    int t = arr[i + gap];
+                    arr[i + gap] = arr[i];
+                    arr[i] = t;
+                }
+            }
+        }
+    }
+}
